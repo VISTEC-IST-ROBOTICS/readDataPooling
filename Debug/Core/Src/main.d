e45494125b7e4cf5c79afe5da9746100699c478d
JK-Pair <thipawan2112@gmail.com>
2023-10-16 20:48:56 +0700

    fifo

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -26,7 +26,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/VISEN_SPI/Lib/IIS3DWB-PID/iis3dwb_reg.h
+ C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/readDataPooling/Lib/IIS3DWB-PID/iis3dwb_reg.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -55,4 +55,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
-C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/VISEN_SPI/Lib/IIS3DWB-PID/iis3dwb_reg.h:
+C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/readDataPooling/Lib/IIS3DWB-PID/iis3dwb_reg.h:
--- Debug/Lib/IIS3DWB-PID/subdir.mk
+++ Debug/Lib/IIS3DWB-PID/subdir.mk
@@ -16,7 +16,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Lib/IIS3DWB-PID/%.o Lib/IIS3DWB-PID/%.su Lib/IIS3DWB-PID/%.cyclo: ../Lib/IIS3DWB-PID/%.c Lib/IIS3DWB-PID/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/VISEN_SPI/Lib/IIS3DWB-PID" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/Lenovo/STM32CubeIDE/workspace_1.10.1/readDataPooling/Lib/IIS3DWB-PID" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Lib-2f-IIS3DWB-2d-PID
 
